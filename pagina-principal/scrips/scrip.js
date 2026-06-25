de062807d230ccc930106d367fb1583249e2506e
samuel-tolentino-viana <tolentinovianasamuel@gmail.com>
2026-06-25 18:37:00 -0300

    melhoras

--- pagina-principal/scrips/scrip.js
+++ pagina-principal/scrips/scrip.js
@@ -1,9 +1,14 @@
 const fotos = document.querySelectorAll('.fotos');
 
 function crecer(evento) {
-    evento.target.classList.toggle('aumenta');
+    evento.target.classList.add('aumenta');
+}
+
+function diminuir(evento) {
+    evento.target.classList.remove('aumenta');
 }
 
 for (let n = 0; n < fotos.length; n++) {
     fotos[n].addEventListener('click', crecer);
+    fotos[n].addEventListener('mouseout', diminuir)
 }
